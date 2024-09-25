2f4dce0fa7573eaa59300c2802ff057b0ac75b5e
seoyun0311 <138513591+seoyun0311@users.noreply.github.com>
2024-09-26 03:51:15 +0900

    주문 목록 검색, 취소

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -24,7 +24,7 @@ public class OrderRepository {
 
     public List<Order> findAll(OrderSearch orderSearch) {
         //language = JPAQL
-        String jpql = "select o From Order o koin o.member m";
+        String jpql = "select o From Order o join o.member m";
         boolean isFirstCondition = true;
 
         //주문 상태 검색
--- src/main/java/jpabook/jpashop/service/OrderService.java
+++ src/main/java/jpabook/jpashop/service/OrderService.java
@@ -5,9 +5,13 @@ import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import jpabook.jpashop.repository.MemberRepository;
 import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
@@ -56,9 +60,8 @@ public class OrderService {
     }
 
     //검색
-    /*
     public List<Order> findOrders(OrderSearch ordersearch) {
-        return orderRepository.findAll(orderSearch);
+        return orderRepository.findAll(ordersearch);
     }
-     */
+
 }
